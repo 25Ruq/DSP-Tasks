8102bf713660ac407793e81fe5ae54c02ae0f810
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2023-12-19 23:33:36 +0200

    Task 9 Convolution

--- Task9/functions.py
+++ Task9/functions.py
@@ -15,6 +15,7 @@ def fast_correlation(guiobj):
     else:
         print("Only 1 or 2 signals are allowed")
         return
+    print(first_signal)
     first_signal = dft(first_signal)
     second_signal = dft(second_signal)
     N = first_signal.count
@@ -37,5 +38,43 @@ def fast_correlation(guiobj):
     Compare_Signals(guiobj.outfile_name, result_signal.indices, result_signal.samples)
 
 
+# Function to calculate the frequency domain indices
+
 def fast_convolution(guiobj):
-    pass
+    if len(guiobj.lst) == 1:
+        sig1 = sig2 = guiobj.lst[0]
+    elif len(guiobj.lst) == 2:
+        sig1 = guiobj.lst[0]
+        sig2 = guiobj.lst[1]
+    else:
+        print("Only 1 or 2 signals are allowed")
+        return
+
+    N1 = sig1.count
+    N2 = sig2.count
+    M = N1+N2-1
+
+    # Append zeros to both signals
+    sig1.samples = np.append(sig1.samples , np.zeros(M - N1))
+    sig2.samples = np.append(sig2.samples , np.zeros(M - N2))
+
+    # Perform DFT on both signals
+    signal1_dft = dft(sig1)
+    signal2_dft = dft(sig2)
+
+    # Multiply harmonics in the frequency domain
+    product_dft = Signal()
+    product_dft.store_signal(1, 0, M, np.array(range(0, M, 1)),sig2.samples)
+
+    # Make sure that the arrays have the same length by appending zeros if necessary
+    if len(signal1_dft.samples) < len(signal2_dft.samples):
+        signal1_dft.samples = np.append(signal1_dft.samples, np.zeros(len(signal2_dft.samples) - len(signal1_dft.samples)))
+    elif len(signal2_dft.samples) < len(signal1_dft.samples):
+        signal2_dft.samples = np.append(signal2_dft.samples, np.zeros(len(signal1_dft.samples) - len(signal2_dft.samples)))
+
+    product_dft.samples = np.array(signal1_dft.samples) * np.array(signal2_dft.samples)
+
+    # Perform IDFT on the product signal
+    product_signal = idft(product_dft)
+
+    print(product_signal.samples)
